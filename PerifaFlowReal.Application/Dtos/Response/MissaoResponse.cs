ab82c3e8d65cf58ee792b92184bec3b31486d555
victorhugo55 <davichugopereira@gmail.com>
2025-11-21 23:55:47 -0300

    Feat: Create MissaoResponse

---
+++ PerifaFlowReal.Application/Dtos/Response/MissaoResponse.cs
@@ -0,0 +1,18 @@
+﻿namespace PerifaFlowReal.Application.Dtos.Response;
+
+public class MissaoResponse
+{
+    public Guid Id { get; set; }
+    public string Titulo { get; set; }
+    public string Descricao { get; set; }
+    public Guid TrilhaId { get; set; }
+
+    public MissaoResponse(Guid id, string titulo, string descricao, Guid trilhaId)
+    {
+        Id = id;
+        Titulo = titulo;
+        Descricao = descricao;
+        TrilhaId = trilhaId;
+        
+    }
+}
