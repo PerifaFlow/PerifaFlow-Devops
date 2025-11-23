257d32f458abbc473bdd9ddd9c660d2c8d4bda69
victorhugo55 <davichugopereira@gmail.com>
2025-11-23 00:57:53 -0300

    Feat:Create migrations

--- PerifaFlowReal.Infastructure/DependenciInjection.cs
+++ PerifaFlowReal.Infastructure/DependenciInjection.cs
@@ -3,6 +3,8 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PerifaFlowReal.Application.Configs;
 using PerifaFlowReal.Application.Interfaces.Repositories;
+using PerifaFlowReal.Application.Interfaces.Services;
+using PerifaFlowReal.Infastructure.Java;
 using PerifaFlowReal.Infastructure.Percistence.Context;
 using PerifaFlowReal.Infastructure.Percistence.Repositories;
 
@@ -16,6 +18,12 @@ public static class DependenciInjection
         { 
             options.UseOracle(configuration.PerifaFlowDb);
         });
+    }
+
+    private static IServiceCollection AddServices(this IServiceCollection services)
+    {
+        services.AddHttpClient<IRitimoService, RitmoService>();
+        return services;
     }
     
     private static IServiceCollection AddRepositories(this IServiceCollection services)
@@ -32,6 +40,7 @@ public static class DependenciInjection
     {
         services.AddDBContext(settings.ConnectionStrings);
         services.AddRepositories();
+        services.AddServices();
         return services;
     }
 
---
+++ PerifaFlowReal.Infastructure/Migrations/20251123014340_Initial.cs
@@ -0,0 +1,182 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PerifaFlowReal.Infastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Trilha",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    Titulo = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Trilha", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Users",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    Username = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Email = table.Column<string>(type: "NVARCHAR2(50)", maxLength: 50, nullable: false),
+                    Password = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Users", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Missao",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    Titulo = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Descricao = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    TrilhaId = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Missao", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Missao_Trilha_TrilhaId",
+                        column: x => x.TrilhaId,
+                        principalTable: "Trilha",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Portfolios",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    Titulo = table.Column<string>(type: "NVARCHAR2(100)", maxLength: 100, nullable: false),
+                    Url = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UserID = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Portfolios", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Portfolios_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Entrega",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    Tipo = table.Column<int>(type: "NUMBER(10)", nullable: false),
+                    ConteudoUrl = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    PortfolioId = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    UserID = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    MissaoID = table.Column<Guid>(type: "RAW(16)", nullable: false),
+                    CreatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    CreatedBy = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false),
+                    UpdatedAt = table.Column<DateTime>(type: "TIMESTAMP(7)", nullable: false),
+                    UpdatedBy = table.Column<string>(type: "NVARCHAR2(2000)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Entrega", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Entrega_Missao_MissaoID",
+                        column: x => x.MissaoID,
+                        principalTable: "Missao",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Entrega_Portfolios_PortfolioId",
+                        column: x => x.PortfolioId,
+                        principalTable: "Portfolios",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Entrega_Users_UserID",
+                        column: x => x.UserID,
+                        principalTable: "Users",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entrega_MissaoID",
+                table: "Entrega",
+                column: "MissaoID",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entrega_PortfolioId",
+                table: "Entrega",
+                column: "PortfolioId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Entrega_UserID",
+                table: "Entrega",
+                column: "UserID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Missao_TrilhaId",
+                table: "Missao",
+                column: "TrilhaId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Portfolios_UserID",
+                table: "Portfolios",
+                column: "UserID",
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Entrega");
+
+            migrationBuilder.DropTable(
+                name: "Missao");
+
+            migrationBuilder.DropTable(
+                name: "Portfolios");
+
+            migrationBuilder.DropTable(
+                name: "Trilha");
+
+            migrationBuilder.DropTable(
+                name: "Users");
+        }
+    }
+}
